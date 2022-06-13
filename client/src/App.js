531978aa619a93cf71373fde48ce650385bd7090
Ccarit0 <carolinacastilloandrad@gmail.com>
2022-06-13 19:47:09 -0300

    first modification FRONT

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import * as React from 'react'
+import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { landingPage } from '../src/JSfiles/landingPage';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <h1>Welcome to my PokePage</h1>
+      <BrowserRouter>
+      <nav className='LinksNav'>
+      <Link to={"/"} className={"Link"}>GO!</Link>
+      </nav>
+        <Routes>
+          <Route>
+            <Route exact path='/' element={<landingPage />}></Route>
+            <Route></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/JSfiles/landingPage.js
@@ -0,0 +1,9 @@
+
+export const landingPage = () =>{
+    return (
+        <div className="PokePage">
+            <h1 className="tituloLanding">Welcome to my PokePage</h1>
+            <img className="imgLanding" alt="images/landingPage1.jpg"></img>
+        </div>
+    )
+}
